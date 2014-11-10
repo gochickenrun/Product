5cb9ce2094647e261f701bda5309e1573995a3cb
gochickenrun <1297831957@qq.com>
2014-11-10 19:59:16 +0800

    新项目

---
+++ Tent/Tent/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tent
+//
+//  Created by 陈永 on 14-11-10.
+//  Copyright (c) 2014年 w3c. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tent/Tent/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tent
+//
+//  Created by 陈永 on 14-11-10.
+//  Copyright (c) 2014年 w3c. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
